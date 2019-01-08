f6b123af4df26e03ef36e3e7871e3c632ebc6912
Gwendolyn Zubatch <gyzubatch@gmail.com>
2019-01-08 11:12:14 -0800

    corrected response value in QAerospace

--- AboutMe/Program.cs
+++ AboutMe/Program.cs
@@ -8,6 +8,8 @@ namespace AboutMe
         {
             int[] results = { 0, 0 };
             results = QuizMgr(results);
+            Console.WriteLine($"\nYou answered {results[0]} questions correctly and {results[1]} questions incorrectly.");
+            Console.ReadLine();
         }
 
         static int[] QuizMgr(int[] results)
@@ -22,6 +24,7 @@ namespace AboutMe
             }
             // Q1 feedback
             Console.WriteLine("I earned a BS in Mechanical Engineering from The University of Texas at Austin.");
+            Console.WriteLine($"correct: {results[0]}    incorrect: {results[1]}");
             Console.ReadLine();
 
             if (QAerospace() == false)
@@ -34,6 +37,7 @@ namespace AboutMe
             }
             // Q2 feedback
             Console.WriteLine("I've had a pretty full career. In addition to aerospace, I've worked as a designer/engineer in industrial food processing equipment, silicon wafer processing products, military specialty tools research and development, and heavy equipment.");
+            Console.WriteLine($"correct: {results[0]}    incorrect: {results[1]}");
             Console.ReadLine();
 
             if (QYearsAtBoeing() == 11)
@@ -46,6 +50,7 @@ namespace AboutMe
             }
             // Q3 feedback
             Console.WriteLine("I worked at Boeing for just over 11 years in total. Although I grew beyond Boeing, I wouldn't trade that experience for the world.");
+            Console.WriteLine($"correct: {results[0]}    incorrect: {results[1]}");
             Console.ReadLine();
 
             if (QCFGrad().ToLower() == "march")
@@ -58,6 +63,7 @@ namespace AboutMe
             }
             // Q4 feedback
             Console.WriteLine("I will present with my final project group on March 15, and I'll be a proud Code Fellows .NET graduate a few days after!");
+            Console.WriteLine($"correct: {results[0]}    incorrect: {results[1]}");
             Console.ReadLine();
 
             if (QRetire() == 2026)
@@ -70,6 +76,7 @@ namespace AboutMe
             }
             // Q5 feedback
             Console.WriteLine("I fully intend to retire early in 7 years, just after my birthday in 2026.");
+            Console.WriteLine($"correct: {results[0]}    incorrect: {results[1]}");
             Console.ReadLine();
 
             // returns array with [correct, incorrect]
@@ -79,7 +86,7 @@ namespace AboutMe
         static bool QDegree()
         {
             bool response = false; // yes=true, no=false (incorrect false is default)
-            Console.WriteLine("Have I earned a bachelor's degree? (Please enter 'yes' or 'no'.)");
+            Console.WriteLine("\nHave I earned a bachelor's degree? (Please enter 'yes' or 'no'.)");
             string rawResponse = Console.ReadLine();
             if(rawResponse.ToLower() == "yes" || rawResponse.ToLower() == "y")
             {
@@ -92,11 +99,11 @@ namespace AboutMe
         static bool QAerospace()
         {
             bool response = true; // yes=true, no=false (incorrect true is default)
-            Console.WriteLine("As an engineer, have I worked only in aerospace? (Please enter 'yes' or 'no'.)");
+            Console.WriteLine("\nAs an engineer, have I worked only in aerospace? (Please enter 'yes' or 'no'.)");
             string rawResponse = Console.ReadLine();
             if (rawResponse.ToLower() == "no" || rawResponse.ToLower() == "n")
             {
-                response = true;
+                response = false;
             }
             return response;
         }
@@ -104,7 +111,7 @@ namespace AboutMe
         static int QYearsAtBoeing()
         {
             int response = 0; // number of years (incorrect 0 is default)
-            Console.WriteLine("How many (whole) years did I work at Boeing? (Please enter a number.)");
+            Console.WriteLine("\nHow many (whole) years did I work at Boeing? (Please enter a number.)");
             string rawResponse = Console.ReadLine();
             try
             {
@@ -120,7 +127,7 @@ namespace AboutMe
         static string QCFGrad()
         {
             string response = ""; // name of month (incorrect "" is default)
-            Console.WriteLine("During which month in 2019 will I graduate from Code Fellows?");
+            Console.WriteLine("\nDuring which month in 2019 will I graduate from Code Fellows?");
             string rawResponse = Console.ReadLine();
             response = rawResponse;
             return response;
@@ -129,7 +136,7 @@ namespace AboutMe
         static int QRetire()
         {
             int response = 2019; // year (incorrect 2019 is default)
-            Console.WriteLine("In what year do I intend to retire? (Please enter a numeric year.");
+            Console.WriteLine("\nIn what year do I intend to retire? (Please enter a numeric year.)");
             string rawResponse = Console.ReadLine();
             try
             {
