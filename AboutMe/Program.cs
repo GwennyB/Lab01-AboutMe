1885e01ab3f6cd3048c0e2a9ef642b6b971cd89a
Gwendolyn Zubatch <gyzubatch@gmail.com>
2019-01-08 10:30:08 -0800

    added response variables and return statements

--- AboutMe/Program.cs
+++ AboutMe/Program.cs
@@ -17,29 +17,44 @@ namespace AboutMe
             return results;
         }
 
-        static bool Q1()
+        static bool QDegree()
         {
-            
+            bool response; // yes=true, no=false
+            Console.WriteLine("Have I earned a bachelor's degree? (Please enter 'yes' or 'no'");
+            string response = Console.ReadLine();
+            return response;
         }
 
-        static bool Q2()
+        static bool QAerospace()
         {
-
+            bool response; // yes=true, no=false
+            Console.WriteLine("As an engineer, have I worked only in aerospace? (Please enter 'yes' or 'no'");
+            string response = Console.ReadLine();
+            return response;
         }
 
-        static int Q3()
+        static int QYearsAtBoeing()
         {
-
+            int response; // number of years
+            Console.WriteLine("How many (whole) years did I work at Boeing? (Please enter a number. You have 3 guesses.");
+            string response = Console.ReadLine();
+            return response;
         }
 
-        static int Q4()
+        static string QCFGrad()
         {
-
+            string response; // name of month
+            Console.WriteLine("During which month in 2019 will I graduate from Code Fellows?");
+            string response = Console.ReadLine();
+            return response;
         }
 
-        static int Q5()
+        static int QRetire()
         {
-
+            int response; // year
+            Console.WriteLine("In what year do I intend to retire?");
+            string response = Console.ReadLine();
+            return response;
         }
 
     }
