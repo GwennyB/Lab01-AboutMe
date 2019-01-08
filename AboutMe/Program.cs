b4f1065f594d5e9bbef987858d8b3b2b6c148fa5
Gwendolyn Zubatch <43423186+GwennyB@users.noreply.github.com>
2019-01-08 12:11:26 -0800

    Update Program.cs

--- AboutMe/Program.cs
+++ AboutMe/Program.cs
@@ -6,14 +6,16 @@ namespace AboutMe
     {
         static void Main(string[] args)
         {
-            int[] results = { 0, 0 };
-            results = QuizMgr(results);
-            Console.WriteLine($"\nYou answered {results[0]} questions correctly and {results[1]} questions incorrectly.");
-            Console.ReadLine();
+            int[] results = { 0, 0 }; // establish results tallying array
+            results = QuizMgr(results); // launch QuizMgr
+            Console.WriteLine($"\nYou answered {results[0]} questions correctly and {results[1]} questions incorrectly."); // final results
+            Console.ReadLine(); // wait for the user to be done reading
         }
 
+        // QuizMgr acts as an intermediary between Main and the user. It initiates each quiz question, tallies results to return to Main, and gives more details on each topic.
         static int[] QuizMgr(int[] results)
         {
+            // launch question
             if (QDegree() == true)
             {
                 results[0]++;
@@ -28,6 +30,7 @@ namespace AboutMe
             Console.WriteLine("I earned a BS in Mechanical Engineering from The University of Texas at Austin.");
             Console.ReadLine();
 
+            // launch question
             if (QAerospace() == false)
             {
                 results[0]++;
@@ -42,6 +45,7 @@ namespace AboutMe
             Console.WriteLine("I've had a pretty full career. In addition to aerospace, I've worked as a designer/engineer in industrial food processing equipment, silicon wafer processing products, military specialty tools research and development, and heavy equipment.");
             Console.ReadLine();
 
+            // launch question
             if (QYearsAtBoeing() == 11)
             {
                 results[0]++;
@@ -56,6 +60,7 @@ namespace AboutMe
             Console.WriteLine("I worked at Boeing for just over 11 years in total. Although I grew beyond Boeing, I wouldn't trade that experience for the world.");
             Console.ReadLine();
 
+            // launch question
             if (QCFGrad().ToLower() == "march")
             {
                 results[0]++;
@@ -70,6 +75,7 @@ namespace AboutMe
             Console.WriteLine("I will present with my final project group on March 15, and I'll be a proud Code Fellows .NET graduate a few days after!");
             Console.ReadLine();
 
+            // launch question
             if (QRetire() == 2026)
             {
                 results[0]++;
@@ -88,6 +94,7 @@ namespace AboutMe
             return results;
         }
 
+        // question with response conditioning
         static bool QDegree()
         {
             bool response = false; // yes=true, no=false (incorrect false is default)
@@ -101,6 +108,7 @@ namespace AboutMe
             return response;
         }
 
+        // question with response conditioning
         static bool QAerospace()
         {
             bool response = true; // yes=true, no=false (incorrect true is default)
@@ -113,6 +121,7 @@ namespace AboutMe
             return response;
         }
 
+        // question with response conditioning and data type validation
         static int QYearsAtBoeing()
         {
             int response = 0; // number of years (incorrect 0 is default)
@@ -129,6 +138,7 @@ namespace AboutMe
             return response;
         }
 
+        // question with response conditioning
         static string QCFGrad()
         {
             string response = ""; // name of month (incorrect "" is default)
@@ -138,6 +148,7 @@ namespace AboutMe
             return response;
         }
 
+        // question with response conditioning and data type validation
         static int QRetire()
         {
             int response = 2019; // year (incorrect 2019 is default)
