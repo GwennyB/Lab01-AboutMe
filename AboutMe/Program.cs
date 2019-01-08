8d88975ce44f5a91f61900e2eae3f189fab7a231
Gwendolyn Zubatch <gyzubatch@gmail.com>
2019-01-08 10:38:15 -0800

    set defaults for all responses

--- AboutMe/Program.cs
+++ AboutMe/Program.cs
@@ -32,7 +32,7 @@ namespace AboutMe
 
         static bool QAerospace()
         {
-            bool response; // yes=true, no=false
+            bool response = true; // yes=true, no=false (incorrect is default)
             Console.WriteLine("As an engineer, have I worked only in aerospace? (Please enter 'yes' or 'no'");
             string rawResponse = Console.ReadLine();
             return response;
@@ -40,7 +40,7 @@ namespace AboutMe
 
         static int QYearsAtBoeing()
         {
-            int response; // number of years
+            int response = 0; // number of years (incorrect 0 is default)
             Console.WriteLine("How many (whole) years did I work at Boeing? (Please enter a number. You have 3 guesses.");
             string rawResponse = Console.ReadLine();
             return response;
@@ -48,7 +48,7 @@ namespace AboutMe
 
         static string QCFGrad()
         {
-            string response; // name of month
+            string response = ""; // name of month (incorrect "" is default)
             Console.WriteLine("During which month in 2019 will I graduate from Code Fellows?");
             string rawResponse = Console.ReadLine();
             return response;
@@ -56,7 +56,7 @@ namespace AboutMe
 
         static int QRetire()
         {
-            int response; // year
+            int response = 2019; // year (incorrect 2019 is default)
             Console.WriteLine("In what year do I intend to retire?");
             string rawResponse = Console.ReadLine();
             return response;
