88ff0334183f1f21664c7e052820275212ceb14d
Gwendolyn Zubatch <gyzubatch@gmail.com>
2019-01-08 10:23:43 -0800

    scaffolded

---
+++ AboutMe/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace AboutMe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] results = { 0, 5 };
+            results = QuizMgr(results);
+        }
+
+        static int[] QuizMgr(int[] results)
+        {
+            
+            // returns array with [correct, incorrect]
+            return results;
+        }
+
+        static bool Q1()
+        {
+            
+        }
+
+        static bool Q2()
+        {
+
+        }
+
+        static int Q3()
+        {
+
+        }
+
+        static int Q4()
+        {
+
+        }
+
+        static int Q5()
+        {
+
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Lab01-AboutMe
+## Lab01-AboutMe ##
+
+
+# What is the purpose of the program?
+This program is a Console-based quiz. When run, it asks a series of questions, pausing after each to receive responses from the user, and giving feedback to the user after each response.
+
+# How do I run the program?
+This is a Console-based program. To run in Visual Studio 2017, load the project (AboutMe/AboutMe.csproj), open the code file (AboutMe/Project.cs), and press Ctrl+F5 to compile and run without debug. The quiz will automatically run in a new terminal window.
+
+# What does the program look like? (visual)
